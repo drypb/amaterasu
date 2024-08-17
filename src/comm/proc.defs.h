5e14764df6225e1a57fd58dec84b8d7f470c94d0
bpt <bernardo.p.tomasi@gmail.com>
2024-08-17 17:33:33 -0300

    IF_SUCCESS macro description corrected

--- src/comm/proc.defs.h
+++ src/comm/proc.defs.h
@@ -7,7 +7,7 @@
  *  (i.e., a status code indicating failure), the macro will short-circuit and
  *  stop executing further calls.
  *
- *  The 'IF_SUCCESS' macro the implementation specific for each module. In
+ *  The 'IF_SUCCESS' macro is implementation specific for each module. In
  *  this case, it is made only for the fields of the 'PROC' struct.
  */
 #define IF_SUCCESS(Status, F1, F2, F3, F4, F5)              \
