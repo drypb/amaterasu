df5101bee49fc71991cf6180845a7fb44eafa8f2
bpt <bernardo.p.tomasi@gmail.com>
2024-08-19 16:31:06 -0300

    IF_SUCCESS recursive declaration

--- src/comm/comm.defs.h
+++ src/comm/comm.defs.h
@@ -9,30 +9,31 @@
 #define _PoolType_      __drv_strictTypeMatch(__drv_typeExpr)
 
 #define PP_RSEQ_N()                                         \
-         63,62,61,60,                                       \
-         59,58,57,56,55,54,53,52,51,50,                     \
-         49,48,47,46,45,44,43,42,41,40,                     \
-         39,38,37,36,35,34,33,32,31,30,                     \
-         29,28,27,26,25,24,23,22,21,20,                     \
-         19,18,17,16,15,14,13,12,11,10,                     \
-         9,8,7,6,5,4,3,2,1,0
+        63,62,61,60,                                        \
+        59,58,57,56,55,54,53,52,51,50,                      \
+        49,48,47,46,45,44,43,42,41,40,                      \
+        39,38,37,36,35,34,33,32,31,30,                      \
+        29,28,27,26,25,24,23,22,21,20,                      \
+        19,18,17,16,15,14,13,12,11,10,                      \
+        9,8,7,6,5,4,3,2,1,0
 
 
 #define PP_ARG_N(                                           \
-          _1, _2, _3, _4, _5, _6, _7, _8, _9,_10,           \
-         _11,_12,_13,_14,_15,_16,_17,_18,_19,_20,           \
-         _21,_22,_23,_24,_25,_26,_27,_28,_29,_30,           \
-         _31,_32,_33,_34,_35,_36,_37,_38,_39,_40,           \
-         _41,_42,_43,_44,_45,_46,_47,_48,_49,_50,           \
-         _51,_52,_53,_54,_55,_56,_57,_58,_59,_60,           \
-         _61,_62,_63,N,...) N
+        _1, _2, _3, _4, _5, _6, _7, _8, _9,_10,             \
+        _11,_12,_13,_14,_15,_16,_17,_18,_19,_20,            \
+        _21,_22,_23,_24,_25,_26,_27,_28,_29,_30,            \
+        _31,_32,_33,_34,_35,_36,_37,_38,_39,_40,            \
+        _41,_42,_43,_44,_45,_46,_47,_48,_49,_50,            \
+        _51,_52,_53,_54,_55,_56,_57,_58,_59,_60,            \
+        _61,_62,_63,N,...) N
+
 
 #define PP_NARG_(...)                                       \
-         PP_ARG_N(__VA_ARGS__)
+        PP_ARG_N(__VA_ARGS__)
 
 
 #define PP_NARG(...)                                        \
-         PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
+        PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
 
 
 #define IF_SUCCESS2(Status, F1, F2)                         \
@@ -48,10 +49,7 @@
     do {                                                    \
         Status = F1;                                        \
         if(NT_SUCCESS(Status)) {                            \
-            Status = F2;                                    \
-            if(NT_SUCCESS(Status)) {                        \
-                Status = F3;                                \
-            }                                               \
+            IF_SUCCESS2(Status, F2, F3);                    \
         }                                                   \
     } while(0)
 
@@ -60,13 +58,7 @@
     do {                                                    \
         Status = F1;                                        \
         if(NT_SUCCESS(Status)) {                            \
-            Status = F2;                                    \
-            if(NT_SUCCESS(Status)) {                        \
-                Status = F3;                                \
-                if(NT_SUCCESS(Status)) {                    \
-                    Status = F4;                            \
-                }                                           \
-            }                                               \
+            IF_SUCCESS3(Status, F2, F3, F4);                \
         }                                                   \
     } while(0)
 
@@ -75,16 +67,7 @@
     do {                                                    \
         Status = F1;                                        \
         if(NT_SUCCESS(Status)) {                            \
-            Status = F2;                                    \
-            if(NT_SUCCESS(Status)) {                        \
-                Status = F3;                                \
-                if(NT_SUCCESS(Status)) {                    \
-                    Status = F4;                            \
-                    if(NT_SUCCESS(Status)) {                \
-                        Status = F5;                        \
-                    }                                       \
-                }                                           \
-            }                                               \
+            IF_SUCCESS4(Status, F2, F2, F3, F3, F5);        \
         }                                                   \
     } while(0)
 
@@ -119,7 +102,7 @@
  *      initialization.
  */
 #define IF_SUCCESS(Status, ...)                             \
-    IF_SUCCESS_(Status, __VA_ARGS__)
+    IF_SUCCESS_(Status, PP_NARG(__VA_ARGS__), __VA_ARGS__)
 
 /*
  *  Debug() -
